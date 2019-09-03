d37c1eeb53ebaf2a61d2e40ef082e4aaeebd197b
Hovhannes Mkrtchyan <hovhannes.mkrtchyan92@gmail.com>
2019-09-04 00:17:18 +0400

    test local access

--- app/models/access.js
+++ app/models/access.js
@@ -22,7 +22,7 @@ class access{
 		var query = "SELECT * FROM users WHERE typeAccess='local' and uniqueData='"+username+"'"
 		con.query(query,(err,res)=>{
 			console.log(res)
-			callback(res.rowCount==0)
+			//callback(res.rowCount==0)
 		}) 
 		con.end()
 	}
