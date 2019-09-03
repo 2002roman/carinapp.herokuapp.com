31f064b02f2583349d6b13059ef061dfe1043331
Hovhannes Mkrtchyan <hovhannes.mkrtchyan92@gmail.com>
2019-09-03 22:27:30 +0400

    disconnect pg

--- app/migrations/projects.js
+++ app/migrations/projects.js
@@ -1,15 +1,12 @@
 exports.migration = {
-    up : {
-        'CREATE TABLE projects (\
+    up : 'CREATE TABLE projects (\
              uniqueDataOfUser varchar(255),\
              id varchar(255),\
              projectName varchar(255),\
              status bool,\
              projectData json,\
              configuration json\
-        );'
-    },
-    down : {
-        'Drop table projects;'
-    }
+        );',
+    down :'Drop table projects;',
+    refresh : 'Delete from projects'
 }
--- app/migrations/users.js
+++ app/migrations/users.js
@@ -1,15 +1,12 @@
 exports.migration = {
-    up : {
-        'CREATE TABLE users (\
+    up : 'CREATE TABLE users (\
              uniqueData varchar(255),\
              name varchar(255),\
              lastname varchar(255),\
              token varchar(255),\
              passowrd varchar(255),\
              typeAccess varchar(255)\
-        );'
-    },
-    down : {
-        'Drop table users;'
-    }
+        );',
+    down : 'Drop table users;',
+    refresh : 'Delete from users'
 }
