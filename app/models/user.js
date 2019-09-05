7ae924fa2b28567902a7345d3aaca2a43b282f1c
Hovhannes Mkrtchyan <hovhannes.mkrtchyan92@gmail.com>
2019-09-05 17:25:24 +0400

    debuging

--- app/models/user.js
+++ app/models/user.js
@@ -61,7 +61,7 @@ class user{
 	createProject(cookies,data,id,callback){
 		this.getUserUniqueDataWithToken(cookies.typeAccess,cookies.token,(uniqueData)=>{
 			const con = new Pool(config.pgCon);
-			var query = "INSERT INTO project(uniqueDataOfUser, id, projectName, status, projectData, configuration) VALUES ($1, $2, $3, $4, $5, $6)"
+			var query = "INSERT INTO projects(uniqueDataOfUser, id, projectName, status, projectData, configuration) VALUES ($1, $2, $3, $4, $5, $6)"
 			var values = [ uniqueData, id, data.projectName, false, data ,{}];
 			con.query(query,values)
 			con.end()	
