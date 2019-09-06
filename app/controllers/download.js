da46bb63fab126ec1c354b58cbad5387962475da
Hovhannes Mkrtchyan <hovhannes.mkrtchyan92@gmail.com>
2019-09-06 19:24:00 +0400

    correcting server side

--- app/controllers/download.js
+++ app/controllers/download.js
@@ -1,5 +1,5 @@
 const config = require('../../config/setting.js')
 
 exports.robotZip = (req,res)=>{
-    res.sendFile(__dirname+'../../download/robot.rar')
+    res.download(__dirname+'../../download/robot.rar','robot.rar')
 }
