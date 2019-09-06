7f5a06a3086ad545ca91399664b9acca7c8ddf14
Hovhannes Mkrtchyan <hovhannes.mkrtchyan92@gmail.com>
2019-09-06 19:29:06 +0400

    debuging

--- app/controllers/download.js
+++ app/controllers/download.js
@@ -1,6 +1,6 @@
 const config = require('../../config/setting.js')
 
 exports.robotZip = (req,res)=>{
-    console.log('filename:',__dirname+'../../download/robot.rar')
+    console.log('filename:',require('path').join(__dirname, 'download/robot.rar'))
     res.download(__dirname+'../../download/robot.rar','robot.rar')
 }
