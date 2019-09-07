48c410c36f8db9f46761b72f44633ba68c571c90
Hovhannes Mkrtchyan <hovhannes.mkrtchyan92@gmail.com>
2019-09-07 19:59:29 +0400

    debuging

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -28,9 +28,7 @@ exports.downloadProject = (req,res)=>{
 	user.project(req.cookies,req.params.id,(project)=>{
 		fs.writeFile(req.params.id+'---project.json',JSON.stringify(project),(err)=>{
 	    	res.download(req.params.id+'---project.json','projectData.json',function(errorDownoad){
-	    		fs.unlink(req.params.id+'---project.json',()=>{
-	    			res.redirect('back'||'/')
-	    		});
+	    		fs.unlink(req.params.id+'---project.json',()=>{});
 	    	})
 		})
 	})
