dfd59c15913538280b3d28bc294037d4261b2931
Hovhannes Mkrtchyan <hovhannes.mkrtchyan92@gmail.com>
2019-09-14 14:33:40 +0400

    debuging

--- config/socketIoStream.js
+++ config/socketIoStream.js
@@ -6,6 +6,7 @@ module.exports = function (socketIo, server) {
 
 		ioStream[i].on('connection', function (socket) {
 			socket.on('stream',(data)=>{
+				console.log('stream ::::: ',data)
 				ioStream[i].to(data.id).emit('stream', data.data);
 			})
 		});
