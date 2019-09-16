08bced22f1d63005bce2407d2afb0c3a0294c093
Hovhannes Mkrtchyan <hovhannes.mkrtchyan92@gmail.com>
2019-09-16 22:07:02 +0400

    debuging

--- config/socketIoStream.js
+++ config/socketIoStream.js
@@ -1,7 +1,7 @@
 var user = require('../app/models/user.js')
 var ioStream = []
 module.exports = function (socketIo, server) {
-	for(let i = 0; i < 10; i++){
+	for(let i = 0; i < 20; i++){
 		ioStream.push(socketIo(server, { path: '/stream'+i }))
 
 		ioStream[i].on('connection', function (socket) {
