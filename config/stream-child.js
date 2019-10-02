46c7c976747054b14d357164e738cfcba8ccbf9b
Hovhannes Mkrtchyan <hovhannes.mkrtchyan92@gmail.com>
2019-10-03 00:21:00 +0400

    test heroku child process

--- config/stream-child.js
+++ config/stream-child.js
@@ -7,13 +7,13 @@ process.on('message', (msg) => {
 });
 
 let startSocket = (con)=>{
-	// console.log(con.socketIo,con.server,con.index)
-	console.log('started socket stream in index :',con.index)
-	let ioStream = con.socketIo(con.server, { path: '/stream'+con.index })
+	console.log(con)
+	// console.log('started socket stream in index :',con.index)
+	// let ioStream = con.socketIo(con.server, { path: '/stream'+con.index })
 
-	ioStream.on('connection', function (socket) {
-		socket.on('stream',(data)=>{
-			ioStream.to(data.id).emit('stream', data.image);
-		})
-	});
+	// ioStream.on('connection', function (socket) {
+	// 	socket.on('stream',(data)=>{
+	// 		ioStream.to(data.id).emit('stream', data.image);
+	// 	})
+	// });
 }
